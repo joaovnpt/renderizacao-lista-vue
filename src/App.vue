b47c442c9ba101885c059615b8f993c419799745
joaovnpt <joaovitorguiotti8@gmail.com>
2024-03-11 15:10:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, ref } from 'vue'
+const cidades = ref([
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília'
+])
+const cidadesSort = computed(() => cidades.value.sort())
+</script>
+
+<template>
+  <ul>
+    <li v-for="(cidade, index) in cidadesSort" :key="index">{{ cidade }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
